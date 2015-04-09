6a786136ab5372ab26b97c4d734710a8180fdb51
pwl <pwl@pwl.pl>
2015-04-09 19:23:12 +0200

    WynikServiceTest in fact fixed ;)

--- opw/src/test/java/com/adamkowalewski/opw/webservice/BaseWynikServiceTest.java
+++ opw/src/test/java/com/adamkowalewski/opw/webservice/BaseWynikServiceTest.java
@@ -1,7 +1,6 @@
 package com.adamkowalewski.opw.webservice;
 
-import com.adamkowalewski.opw.view.controller.KandydatController;
-import com.adamkowalewski.opw.view.controller.OkregowaController;
+import com.adamkowalewski.opw.webservice.controller.WynikEjb;
 import org.glassfish.hk2.api.Factory;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -13,30 +12,28 @@ import javax.ws.rs.core.Application;
 public abstract class BaseWynikServiceTest extends JerseyTestNg.ContainerPerMethodTest {
 
     protected static WynikService wynikService = new WynikService();
-    protected static KandydatController kandydatController = Mockito.mock(KandydatController.class);
-    protected static OkregowaController okregowaController = Mockito.mock(OkregowaController.class);
+    protected static WynikEjb wynikEjb = Mockito.mock(WynikEjb.class);
 
-//    protected static class MockWynikServiceFactory implements Factory<WynikService> {
-//        @Override
-//        public WynikService provide() {
-//            wynikService.kandydatController = kandydatController;
-//            wynikService.okregowaController = okregowaController;
-//            return wynikService;
-//        }
-//
-//        @Override
-//        public void dispose(WynikService wynikService) {
-//            /* Nothing to do here */
-//        }
-//    }
-//
+    protected static class MockWynikServiceFactory implements Factory<WynikService> {
+        @Override
+        public WynikService provide() {
+            wynikService.wynikEjb = wynikEjb;
+            return wynikService;
+        }
+
+        @Override
+        public void dispose(WynikService wynikService) {
+            /* Nothing to do here */
+        }
+    }
+
     @Override
     public Application configure() {
         AbstractBinder binder = new AbstractBinder() {
             @Override
             protected void configure() {
-//                bindFactory(MockWynikServiceFactory.class)
-//                        .to(WynikService.class);
+                bindFactory(MockWynikServiceFactory.class)
+                        .to(WynikService.class);
             }
         };
         ResourceConfig config = new ResourceConfig(WynikService.class);

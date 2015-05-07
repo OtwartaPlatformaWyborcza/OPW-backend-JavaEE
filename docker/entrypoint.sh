7cd436c5f59d1570b542bec7f2d87439d4da49cd
Adam Kowalewski <github@adamkowalewski.com>
2015-05-07 22:27:39 +0000

    docker added admin port

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # sed & copy xml config, zmienic port na ENV PORT0
 cat /opt/glassfish4/glassfish/domains/domain1/config/domain.xml | sed -e "s/OPW_HTTP/${PORT0}/g" > /opt/glassfish4/glassfish/domains/domain1/config/domain.xml ;
-#cat /opt/glassfish4/glassfish/domains/domain1/config/domain.xml | sed -e "s/OPW_ADMIN/${PORT1}/g" > /opt/glassfish4/glassfish/domains/domain1/config/domain.xml ;
+cat /opt/glassfish4/glassfish/domains/domain1/config/domain.xml | sed -e "s/OPW_ADMIN/${PORT1}/g" > /opt/glassfish4/glassfish/domains/domain1/config/domain.xml ;
 
 /opt/glassfish4/glassfish/bin/asadmin start-domain --verbose
 

889eacde692715654ab59a121fd702dca3e143fa
Adam Kowalewski <github@adamkowalewski.com>
2015-05-08 11:56:13 +0200

    DB regenerated entities

---
+++ opw/src/main/java/com/adamkowalewski/opw/entity/OpwSession.java
@@ -0,0 +1,148 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Adam Kowalewski.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.adamkowalewski.opw.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Adam Kowalewski
+ */
+@Entity
+@Table(name = "opw_session", catalog = "opw", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "OpwSession.findAll", query = "SELECT o FROM OpwSession o"),
+    @NamedQuery(name = "OpwSession.findById", query = "SELECT o FROM OpwSession o WHERE o.id = :id"),
+    @NamedQuery(name = "OpwSession.findByToken", query = "SELECT o FROM OpwSession o WHERE o.token = :token"),
+    @NamedQuery(name = "OpwSession.findByDateValidTo", query = "SELECT o FROM OpwSession o WHERE o.dateValidTo = :dateValidTo"),
+    @NamedQuery(name = "OpwSession.findByActive", query = "SELECT o FROM OpwSession o WHERE o.active = :active")})
+public class OpwSession implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Size(max = 64)
+    @Column(name = "token", length = 64)
+    private String token;
+    @Column(name = "dateValidTo")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateValidTo;
+    @Column(name = "active")
+    private Boolean active;
+    @JoinColumn(name = "opw_user_id", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false)
+    private OpwUser opwUserId;
+
+    public OpwSession() {
+    }
+
+    public OpwSession(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getDateValidTo() {
+        return dateValidTo;
+    }
+
+    public void setDateValidTo(Date dateValidTo) {
+        this.dateValidTo = dateValidTo;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public OpwUser getOpwUserId() {
+        return opwUserId;
+    }
+
+    public void setOpwUserId(OpwUser opwUserId) {
+        this.opwUserId = opwUserId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OpwSession)) {
+            return false;
+        }
+        OpwSession other = (OpwSession) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.adamkowalewski.opw.entity.OpwSession[ id=" + id + " ]";
+    }
+    
+}

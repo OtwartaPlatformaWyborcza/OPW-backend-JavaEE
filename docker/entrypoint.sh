791485cfbb985600704e7066279e2c67a8e853d6
Adam Kowalewski <github@adamkowalewski.com>
2015-05-10 01:33:22 +0100

    docker added sed for JMS port

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# sed & copy xml config, zmienic port na ENV PORT0
-cat /opt/glassfish4/glassfish/domains/domain1/config/domain.xml.org | sed -e "s/OPW_HTTP/${PORT0}/g" | sed -e "s/OPW_ADMIN/${PORT1}/g" > /opt/glassfish4/glassfish/domains/domain1/config/domain.xml ;
-#cat /opt/glassfish4/glassfish/domains/domain1/config/domain.xml | sed -e "s/OPW_ADMIN/${PORT1}/g" > /opt/glassfish4/glassfish/domains/domain1/config/domain.xml ;
+# PORT0 - HTTP 
+# PORT1 - admin
+# PORT0 - JMS (auto offset +100)
+cat /opt/glassfish4/glassfish/domains/domain1/config/domain.xml.org | sed -e "s/OPW_HTTP/${PORT0}/g" | sed -e "s/OPW_ADMIN/${PORT1}/g" | sed -e "s/OPW_JMS/${PORT0}/g" > /opt/glassfish4/glassfish/domains/domain1/config/domain.xml ;
 
 /opt/glassfish4/glassfish/bin/asadmin start-domain --verbose
 
